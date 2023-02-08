7301e64ad303960f7511fb53c8b67911f7bd4ada
mohammedsaif001 <mdsaif4online@gmail.com>
2023-02-08 20:41:58 +0530

    Fixing React-Router-Dom

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import { makeStyles } from "@material-ui/core";
-import { BrowserRouter, Route } from 'react-router-dom';
-import './App.css';
-import Header from './Components/Header';
-import CoinPage from './Pages/CoinPage';
-import HomePage from './Pages/HomePage';
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./Components/Header";
+import CoinPage from "./Pages/CoinPage";
+import HomePage from "./Pages/HomePage";
 
 const useStyles = makeStyles({
   App: {
     backgroundColor: "#14161a",
     color: "white",
-    minHeight: '100vh'
+    minHeight: "100vh",
   },
-})
+});
 function App() {
-
-  const classes = useStyles()
-  return (<BrowserRouter>
-    <div className={classes.App}>
-      <Header />
-      <Route path="/" component={HomePage} exact />
-      <Route path="/coins/:id" component={CoinPage} />
-    </div>
-  </BrowserRouter>
+  const classes = useStyles();
+  return (
+    <>
+      <Router>
+        <Switch>
+          <div className={classes.App}>
+            <Header />
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            <Route exact path="/coins/:id">
+              <CoinPage />
+            </Route>
+          </div>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
